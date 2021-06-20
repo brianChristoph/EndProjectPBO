d5e5f56c90a62a7ee7c300621883fc4fb564e3a2
BRCS <BRCS@DESKTOP-T609FHM>
2021-06-20 17:25:06 +0700

    getter setter user

--- src/model/User.java
+++ src/model/User.java
@@ -11,9 +11,41 @@ package model;
  */
 public class User {
     
-    String id;
-    String nama;
-    String password;
-    String noTlp;
+    private String id;
+    private String nama;
+    private String password;
+    private String noTlp;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getNoTlp() {
+        return noTlp;
+    }
+
+    public void setNoTlp(String noTlp) {
+        this.noTlp = noTlp;
+    }
     
 }
