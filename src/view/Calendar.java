d17d299deb76c87982d582b9905d5ebd69378b99
quitsv <axellsilvano29@gmail.com>
2021-07-09 13:52:02 +0700

    Added temporary try catch on calendar for catching error

--- src/view/Calendar.java
+++ src/view/Calendar.java
@@ -13,41 +13,43 @@ import controller.CalendarController;
 public class Calendar {
 
     Calendar() {
-        dummy();
 
         CalendarController controller = new CalendarController();
 
         JFrame f = new JFrame("Schedules");
         f.setSize(432, 768);
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        try {
+            Header header = new Header(UserManager.getInstance().getUser().getNama(), "Calendar");
+            f.add(header.getHeader());
 
-        Header header = new Header(UserManager.getInstance().getUser().getNama(), "Calendar");
-        f.add(header.getHeader());
+            JPanel panel = new JPanel();
 
-        JPanel panel = new JPanel();
+            ArrayList<Kelas> schedules = controller.getKelasFromDB();
 
-        ArrayList<Kelas> schedules = controller.getKelasFromDB();
+            int tambah = 253;
+            int tambahJadwal = 277;
+            for (int i = 0; i < schedules.size(); i++) {
+                JLabel kelas = new JLabel("Kelas " + schedules.get(i).getNama());
+                JLabel jadwal = new JLabel("Jadwal " + schedules.get(i).getJadwal());
+                kelas.setBounds(46, tambah, 108, 12);
+                jadwal.setBounds(70, tambahJadwal, 235, 12);
+                tambah += 60;
+                tambahJadwal += 60;
+                f.add(kelas);
+                f.add(jadwal);
 
-        int tambah = 253;
-        int tambahJadwal = 277;
-        for (int i = 0; i < schedules.size(); i++) {
-            JLabel kelas = new JLabel("Kelas " + schedules.get(i).getNama());
-            JLabel jadwal = new JLabel("Jadwal " + schedules.get(i).getJadwal());
-            kelas.setBounds(46, tambah, 108, 12);
-            jadwal.setBounds(70, tambahJadwal, 235, 12);
-            tambah += 60;
-            tambahJadwal += 60;
-            f.add(kelas);
-            f.add(jadwal);
+            }
+            panel.setVisible(true);
+            panel.setLayout(null);
 
+            JScrollPane scroll = new JScrollPane(panel);
+            f.getContentPane().add(scroll);
+            f.setLayout(null);
+            f.setVisible(true);
+        } catch (NullPointerException e) {
+            ErrorView.printError("no user");
         }
-        panel.setVisible(true);
-        panel.setLayout(null);
-
-        JScrollPane scroll = new JScrollPane(panel);
-        f.getContentPane().add(scroll);
-        f.setLayout(null);
-        f.setVisible(true);
 
 //        JButton button1 = new JButton("Button1");
 //
@@ -60,10 +62,6 @@ public class Calendar {
 //                
 //            }
 //        });
-    }
-
-    void dummy() {
-
     }
 
     public static void main(String[] args) {
