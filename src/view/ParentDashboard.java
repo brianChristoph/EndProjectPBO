912e8a98676fd4f8888a1c23147721a8c19d575f
JoAurelius <85978338+JoAurelius@users.noreply.github.com>
2021-07-11 18:22:21 +0700

    Update ParentDashboard.java

--- src/view/ParentDashboard.java
+++ src/view/ParentDashboard.java
@@ -84,7 +84,4 @@ public class ParentDashboard {
         f.setLayout(null);
         f.setVisible(true);
     }
-    public static void main(String[] args) {
-        new ParentDashboard();
-    }
 }
