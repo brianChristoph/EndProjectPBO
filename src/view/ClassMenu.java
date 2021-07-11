43b1dafed8f9db395694065af206df7a31ffb291
brianChristoph <85978468+brianChristoph@users.noreply.github.com>
2021-07-11 18:50:07 +0700

    Update ClassMenu.java

--- src/view/ClassMenu.java
+++ src/view/ClassMenu.java
@@ -6,15 +6,25 @@
 package view;
 
 import java.awt.Button;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
+import model.Guru;
+import model.Kelas;
+import model.Murid;
+import model.TipeUser;
+import model.User;
+import model.UserManager;
 
 /**
  *
@@ -28,56 +38,64 @@ public class ClassMenu {
         Font roboto = new Font("Roboto", Font.PLAIN, 18);
 
         JLabel identity = new JLabel("Welcome,");
-        identity.setBounds(155, 72, 157, 665);
+        identity.setBounds(155, 72, 130, 32);
         identity.setFont(roboto);
-
-//        ArrayList<Kelas>
-        
-        for (int i = 0; i < 10; i++) {
-            JRadioButton newButton = new JRadioButton("");
-        }
-
-        ButtonGroup bg = new ButtonGroup();
-
-        JLabel id = new JLabel("ID");
-        id.setBounds(63, 370, 20, 16);
-        id.setFont(roboto);
-
-        JTextField inputID = new JTextField();
-        inputID.setBounds(63, 386, 306, 33);
-
-        JLabel password = new JLabel("Password");
-        password.setBounds(63, 439, 100, 16);
-        password.setFont(roboto);
-
-        JPasswordField inputPassword = new JPasswordField();
-        inputPassword.setBounds(63, 455, 306, 33);
-
-        Button login = new Button("Login");
-        login.setBounds(121, 524, 189, 36);
-
-        login.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //Login Controller
-            }
-        });
-
         f.add(identity);
-        f.add(id);
-        f.add(inputID);
-        f.add(password);
-        f.add(inputPassword);
-        f.add(login);
+//        f.getContentPane().setLayout(new FlowLayout());
+//        JScrollPane scrollableTextArea = new JScrollPane(f); 
+//        scrollableTextArea.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+//        f.getContentPane().add(scrollableTextArea);
+        
+        // HardCode Cuz Dbase is not functioning
+//        ArrayList<Kelas> arrKelas = new ArrayList();
+//        Kelas k1 = new Kelas();
+//        k1.setNama("mat");
+//        k1.setJadwal("Senin, 1-2");
+//        Kelas k2 = new Kelas();
+//        k2.setNama("fis");
+//        k2.setJadwal("Selasa, 1-2");
+//        arrKelas.add(k1);
+//        arrKelas.add(k2);
+        
+        User user = UserManager.getInstance().getUser();
+        if(user instanceof Murid){
+            Murid murid = (Murid) user;
+//            murid.setListKelas(arrKelas);
+            f = showListKelas(murid.getListKelas(), f);
+        } else if (user instanceof Guru){
+            System.out.println("hello");
+            Guru guru = (Guru) user;
+//            guru.setAjarKelas(arrKelas);
+            f = showListKelas(guru.getAjarKelas(), f);
+        }
 
         f.setSize(432, 768);
         f.setLayout(null);
         f.setVisible(true);
         
+//        login.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                //Login Controller
+//            }
+//        });
+        
     }
     
-    public static void main(String[] args) {
-        new ClassMenu();
+    private JFrame showListKelas(ArrayList<Kelas> arrK, JFrame frame){
+        for (int i = 0; i < arrK.size(); i++) {
+            if(arrK.get(i) != null){
+                System.out.println("hello");
+                JButton newButton = new JButton(arrK.get(i).getNama());
+                newButton.setBounds(148, 265+(100*i), 108, 20);
+                JLabel newLabel = new JLabel();
+                newLabel.setText(arrK.get(i).getJadwal());
+                newLabel.setBounds(148, 290+(119*i), 235, 20);
+                frame.add(newButton);
+                frame.add(newLabel);
+            }
+        }
+        return frame;
     }
     
 }
