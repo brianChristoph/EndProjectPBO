05acdd0a10ed3463c62628cca635fe040c0f363c
jonat <jonat@Jonathan-PC>
2021-07-11 21:34:25 +0700

    update murid add function to get all murid

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -5,8 +5,10 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import model.Pengumuman;
+import model.User;
 import model.UserManager;
 import model.TipeUser;
+import model.Murid;
 
 /**
  *
@@ -34,5 +36,24 @@ public class MainController {
         }
         return announcements;
     }
-    
+    public ArrayList getAllMurid() {
+        ArrayList<Murid> listMurid = new ArrayList<>();
+        conn.connect();
+        String query = "SELECT * FROM murid";
+        try {
+            Statement st = conn.con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while (rs.next()) {
+                Murid newMurid = new Murid();
+                newMurid.setId(rs.getInt("id_murid"));
+                newMurid.setNIP(rs.getString("nip"));
+                newMurid.setNama(rs.getString("nama"));
+                newMurid.setSPP(rs.getDouble("uang_sekolah"));
+                listMurid.add(newMurid);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return listMurid;
+    }
 }
