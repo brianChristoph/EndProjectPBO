e542db1c0df9cba9abaff12bb34181b0bf03476f
BRCS <BRCS@DESKTOP-T609FHM>
2021-07-11 19:31:39 +0700

    update MuridController

--- src/controller/MuridController.java
+++ src/controller/MuridController.java
@@ -1,56 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controller;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
+import model.Kelas;
 import model.User;
 import model.Guru;
 import model.Murid;
+import model.OrangTua;
 import model.Kelas;
 import model.TipeUser;
+import model.User;
 
 /**
  *
  * @author BRCS
  */
-public class TeacherController {
-
+public class MuridController {
+    
     ArrayList<Kelas> arrKelas = new ArrayList();
     DatabaseHandler conn = new DatabaseHandler();
     
-    // Database related
-    public User getUserData(){
-        Guru user = (Guru)getUser("123", "pass", 0);
-//        System.out.println(user.getNama());
-        return user;
-    }
-    
-    public Guru getUser(String id, String password, int idGuru){
-//    public User getUser(String id, String password, String nama){
+    public Murid getUser(String id, String password, int idMurid){
+        Murid user = new Murid();
         String query = "";
-        Guru user = new Guru();
-        if(idGuru != 0)
-            query = "SELECT * FROM guru WHERE id_guru = '" + idGuru + "'";
+        if(idMurid != 0)
+            query = "SELECT * FROM murid WHERE id_murid = " + idMurid;
         else
             conn.connect();
-            query = "SELECT * FROM guru WHERE nik = '" + id + "' && password = '" + password + "'";
+            query = "SELECT * FROM murid WHERE nip = '" + id + "' && password = '" + password + "'";
         try {
             Statement st = conn.con.createStatement();
             ResultSet rs = st.executeQuery(query);
             while(rs.next()){
+                user.setId(rs.getInt("id_murid"));
+                user.setNIP(rs.getString("nip"));
                 user.setNama(rs.getString("nama"));
-                System.out.println(user.getNama());
                 user.setPassword(rs.getString("password"));
                 user.setNoTlp(rs.getString("no_telepon"));
-                user.setTipe(TipeUser.TEACHER);
+                user.setSPP(rs.getInt("uang_sekolah"));
 //                user.setAjarKelas(getTaughtClasses(rs.getInt("id_guru")));
+                user.setTipe(TipeUser.STUDENT);
             }
         } catch(SQLException ex) {
             ex.printStackTrace();
@@ -75,14 +67,18 @@ public class TeacherController {
         }
         return arrClasses;
     }
-
-    private boolean isTeacher(User pengguna) {
-        if (pengguna instanceof Guru) {
+    
+    private void printError(){
+        
+    }
+    
+    private boolean isMurid(User pengguna){
+        if(pengguna instanceof Murid){
             return true;
         }
         return false;
     }
-
+    
     private int indexKelas(int idKelas) {
         for (int i = 0; i < arrKelas.size(); i++) {
             if (arrKelas.get(i) != null) {
@@ -93,72 +89,22 @@ public class TeacherController {
         }
         return -1;
     }
-
-    private void printError() {
-
-    }
-
-    public void createNewPost(User pengguna, int idKelas, String judul, String post) {
-        if (pengguna.getTipe().TEACHER == TipeUser.TEACHER) {
-            try {
-                String query = "INSERT INTO `post`(`judul`, `deskripsi`) VALUES (?,?)";
-                PreparedStatement st = conn.con.prepareStatement(query);
-                st.setString(1, judul);
-                st.setString(2, post);
-                st.executeUpdate();
-            } catch (SQLException e) {
-
-            }
-        }
-    }
-
-    public void deletePost(User pengguna, int idKelas, int idPost) {
-
-    }
-
-    public void studentAttendance(User pengguna, int idKelas) {
-
-    }
-
-    public void inputGrade(User pengguna, int idKelas, int idMurid, int nilai) {
-
-    }
-
-    public void kickStudent(User pengguna, int idKelas, int idMurid, ArrayList<Murid> murids) {
-        if (isTeacher(pengguna) == true) {
+    
+    public void gabungKelas(User pengguna, int idKelas){
+        if(isMurid(pengguna) == true){
             int idxKelas = indexKelas(idKelas);
-            if (idxKelas != -1) {
-                /**
-                 * get arrMurid yg ada di model kelas delete murid where idMurid
-                 * == arrMurid().get(i).getId()
-                 */
-                int result = getMuridById(murids, idMurid);
-                if (result == -1) {
-                    System.out.println("No murid with id " + idMurid + " found");
-                } else {
-                    try {
-                        String query = "DELETE FROM `murid_kelas` WHERE id_murid = ? AND id_kelas = ?";
-                        PreparedStatement st = conn.con.prepareStatement(query);
-                        st.setInt(1, result);
-                        st.setInt(2, idKelas);
-                        st.executeUpdate();
-                        System.out.println("Murid with id " + idMurid + " deleted from kelas with id " + idKelas);
-                    } catch (SQLException e) {
-                        printError();
-                    }
-                }
+            if(idxKelas != -1){
+                // add pengguna ke arrayList student yang ada di model Kelas
             } else {
                 printError();
             }
         }
     }
 
-    private int getMuridById(ArrayList<Murid> murids, int search) {
-        for (int i = 0; i < murids.size(); i++) {
-            if (murids.get(i).getId() == search) {
-                return murids.get(i).getId();
-            }
+    public void keluarKelas(User pengguna, int idKelas){
+        if(isMurid(pengguna) == true){
+            
         }
-        return -1;
     }
+
 }
