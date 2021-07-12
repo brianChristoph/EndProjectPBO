16ddec97e3decf45244de8bb648262c1d45db876
quitsv <axellsilvano29@gmail.com>
2021-07-12 21:45:23 +0700

    Update report

--- src/view/ReportView.java
+++ src/view/ReportView.java
@@ -43,6 +43,7 @@ public class ReportView {
             f.setLayout(null);
             f.setVisible(true);
         } catch (NullPointerException e) {
+            e.printStackTrace();
             ErrorView.printError("No Data");
         }
 
