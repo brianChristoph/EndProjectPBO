b091aede39cf3be89db5d4bee8cce18624537907
JoAurelius <jonathan.arls@yahoo.com>
2021-07-14 18:59:16 +0700

    Update StudentDashboard.java

--- src/view/StudentDashboard.java
+++ src/view/StudentDashboard.java
@@ -18,54 +18,82 @@ public class StudentDashboard {
 
     public StudentDashboard() {
         JFrame f = new JFrame("Student Dashboard");
-        
-        Header header = new Header("Hi","Jo");
+
+        Header header = new Header("Jo");
         JPanel head = header.getHeader();
-        head.setLocation(0, 0); 
+        head.setLocation(0, 0);
         f.add(head);
-        
+
         head.setLayout(null);
         head.setVisible(true);
         Buttons button = new Buttons();
-        
+
+        f.add(Header.header);
         button.classes.setLocation(56, 230);
         button.classes.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                f.add(Header.header);
-                
+                f.setVisible(false);
+                //new classView
             }
         });
-        
+
         button.calendar.setLocation(243, 230);
         button.calendar.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-//                new CalendarMenu();
+                f.setVisible(false);
+                new CalendarMenu();
             }
         });
-        
+
         button.teachers.setLocation(56, 384);
         button.teachers.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                f.setVisible(false);
                 new TeachersMenu();
             }
         });
         button.report.setLocation(243, 384);
-        button.attandance.setLocation(56, 538);
+        button.report.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f.setVisible(false);
+                new ReportView();
+            }
+        });
+        button.attendance.setLocation(56, 538);
+        button.attendance.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f.setVisible(false);
+                new AttendanceView();
+                
+            }
+        });
         button.announcement.setLocation(243, 538);
-        
-        
+        button.announcement.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                f.setVisible(false);
+                new AnnouncementMenu();
+            }
+        });
+
         f.add(button.classes);
         f.add(button.calendar);
         f.add(button.teachers);
         f.add(button.report);
-        f.add(button.attandance);
+        f.add(button.attendance);
         f.add(button.announcement);
-        
+
         f.setSize(432, 768);
         f.setLayout(null);
         f.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new StudentDashboard();
     }
 }
