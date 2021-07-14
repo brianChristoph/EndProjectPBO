36b6a4c538dd725ba7de01d4e1f56d5850e6cb2c
JoAurelius <jonathan.arls@yahoo.com>
2021-07-14 18:55:34 +0700

    Update ParentController.java

--- src/controller/ParentController.java
+++ src/controller/ParentController.java
@@ -12,7 +12,8 @@ import java.sql.PreparedStatement;
 import model.OrangTua;
 import model.TipeUser;
 import model.User;
-
+import model.Murid;
+import model.UserManager;
 /**
  *
  * @author BRCS
@@ -32,7 +33,7 @@ public class ParentController {
             while(rs.next()){
                 user.setNama(rs.getString("nama"));
                 user.setPassword(rs.getString("password"));
-                user.setNoTlp("no_tlp");
+                user.setNoTlp(rs.getString("no_tlp"));
                 user.setTipe(TipeUser.PARENT);
             }
         } catch(SQLException ex) {
@@ -40,5 +41,25 @@ public class ParentController {
         }
         return user;
     }
-    
+    public Murid getAnak() {
+        Murid anak = new Murid();
+        OrangTua ortu = (OrangTua) UserManager.getInstance().getUser();
+        conn.connect();
+        String query = "SELECT * FROM murid WHERE nip ='" + ortu.getNIP();
+        try {
+            Statement st = conn.con.createStatement();
+            ResultSet rs = st.executeQuery(query);
+            while(rs.next()){
+                anak.setId(rs.getInt("id_murid"));
+                anak.setNama(rs.getString("nama"));
+                anak.setPassword(rs.getString("password"));
+                anak.setNoTlp(rs.getString("no_tlp"));
+                anak.setSPP(rs.getDouble("uang_sekolah"));
+                anak.setTipe(TipeUser.STUDENT);
+            }
+        } catch(SQLException ex) {
+            
+        }
+        return anak;
+    }
 }
