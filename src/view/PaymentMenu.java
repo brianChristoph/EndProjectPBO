7ea6dea503747d87cf64bf3d708e2f4824cce93f
JoAurelius <jonathan.arls@yahoo.com>
2021-07-14 18:59:07 +0700

    Update PaymentMenu.java

--- src/view/PaymentMenu.java
+++ src/view/PaymentMenu.java
@@ -5,6 +5,13 @@
  */
 package view;
 
+import controller.ParentController;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import model.Murid;
+import java.util.Date;
 /**
  *
  * @author jonat
@@ -12,6 +19,35 @@ package view;
 public class PaymentMenu {
 
     public PaymentMenu() {
+        JFrame f = new JFrame("Payment Menu");
+        ParentController c = new ParentController();
+        Murid murid = c.getAnak();
+        double spp = murid.getSPP();
+        Date date = new Date();
+        JLabel title = new JLabel("Tanggal" + date.getMonth());
+        title.setBounds(72,253,108,20);
+        JLabel detail = new JLabel("Total Tagihan : " + spp);
+        detail.setBounds(72,277,235,20);
+        
+        Buttons button = new Buttons();
+        
+        f.add(title);
+        f.add(detail);
+        
+        button.back.setLocation(204,312);
+        f.add(button.back);
+        button.back.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                new ParentDashboard();
+                f.setVisible(false);
+            }
+        });
+        
+        
+        f.setSize(432,768);
+        f.setLayout(null);
+        f.setVisible(true);
     }
     
     
