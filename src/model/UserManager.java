a72bf778cfec61efd1f4f75af3fbd988f9c1f656
JoAurelius <jonathan.arls@yahoo.com>
2021-07-14 18:59:40 +0700

    Revert "Update UserManager.java"
    This reverts commit 66b543bb69071d9d0cad45e5f4c257cce4686249.

--- src/model/UserManager.java
+++ src/model/UserManager.java
@@ -7,7 +7,7 @@ package model;
 public class UserManager {
 
     private static UserManager instance;
-    private User user = new User(1,"asep","password","21",TipeUser.STUDENT){};
+    private User user;
 
     public static UserManager getInstance() {
         if (instance == null) {
