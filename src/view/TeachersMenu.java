59460611cfd4bb4e67ed8092ba5c51e43d09c0ae
JoAurelius <jonathan.arls@yahoo.com>
2021-07-14 18:59:27 +0700

    Update TeachersMenu.java

--- src/view/TeachersMenu.java
+++ src/view/TeachersMenu.java
@@ -5,7 +5,14 @@
  */
 package view;
 
+import controller.MainController;
+import controller.ParentController;
+import java.util.ArrayList;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import model.Guru;
+import model.TipeUser;
+import model.UserManager;
 
 /**
  *
@@ -15,6 +22,32 @@ public class TeachersMenu {
 
     public TeachersMenu() {
        JFrame f = new JFrame();
+        MainController c = new MainController();
+        ParentController pc = new ParentController();
+        int id = 0;
+        if (UserManager.getInstance().getUser().getTipe() == TipeUser.PARENT) {
+            id = pc.getAnak().getId();
+        } else if (UserManager.getInstance().getUser().getTipe() == TipeUser.STUDENT) {
+            id = UserManager.getInstance().getUser().getId();
+        }
+        int tambah = 253;
+        JLabel coloumGuru = new JLabel("Guru");
+        JLabel coloumNoTlp = new JLabel("No Telepon");
+        coloumGuru.setBounds(62,250,220,13);
+        coloumNoTlp.setBounds(296,250,90,13);
+        ArrayList<Guru> guru = c.getTeachersByMurid(id);
+        for (int i = 0; i < guru.size(); i++) {
+            JLabel namaGuru = new JLabel(guru.get(i).getNama());
+            JLabel noTlpGuru = new JLabel(guru.get(i).getNoTlp());
+            namaGuru.setBounds(46, tambah, 108, 12);
+            noTlpGuru.setBounds(70, tambah, 235, 12);
+            tambah += 60;
+            f.add(noTlpGuru);
+            f.add(noTlpGuru);
+        }
+        f.setLayout(null);
+        f.setSize(432,768);
+        f.setVisible(true);
        
     }
     
